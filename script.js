2eda0ee627a31645fdaee604a80bdc38d8799a5f
Trần Mạnh Cường <tmc9012@gmail.com>
2023-03-26 17:26:03 +0700

    vladimir

---
+++ script.js
@@ -0,0 +1,409 @@
+var image1 = document.getElementById("image1");
+var image2 = document.getElementById("image2");
+var image3 = document.getElementById("image3");
+var image4 = document.getElementById("image4");
+var image5 = document.getElementById("image5");
+var image6 = document.getElementById("image6");
+var image7 = document.getElementById("image7");
+var image8 = document.getElementById("image8");
+var image9 = document.getElementById("image9");
+var image10 = document.getElementById("image10");
+var image11 = document.getElementById("image11");
+var image12 = document.getElementById("image12");
+
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3= document.getElementById("button3");
+var button4 = document.getElementById("button4");
+var button5 = document.getElementById("button5");
+var button6 = document.getElementById("button6");
+var button7 = document.getElementById("button7");
+var button8 = document.getElementById("button8");
+var button9 = document.getElementById("button9");
+var button10 = document.getElementById("button10");
+var button11 = document.getElementById("button11");
+var button12 = document.getElementById("button12");
+
+button1.addEventListener("click", function() 
+{
+        image1.style.opacity = "1";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button2.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "1";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button3.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "1";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button4.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "1";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button5.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "1";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button6.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "1";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button7.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "1";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button8.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "1";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button9.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "1";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button10.addEventListener("click", function()
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "1";
+        image11.style.opacity = "0";
+        image12.style.opacity = "0";
+});
+
+button11.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "1";
+        image12.style.opacity = "0";
+});
+
+button12.addEventListener("click", function() 
+{
+        image1.style.opacity = "0";
+        image2.style.opacity = "0";
+        image3.style.opacity = "0";
+        image4.style.opacity = "0";
+        image5.style.opacity = "0";
+        image6.style.opacity = "0";
+        image7.style.opacity = "0";
+        image8.style.opacity = "0";
+        image9.style.opacity = "0";
+        image10.style.opacity = "0";
+        image11.style.opacity = "0";
+        image12.style.opacity = "1";
+});
+
+button1.addEventListener("click", function() 
+{
+        image1.classList.add("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+
+button2.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.add("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+        
+button3.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.add("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+        
+button4.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.add("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+
+button5.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.add("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+        
+button6.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.add("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+
+button7.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.add("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+
+button8.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.add("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+
+button9.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.add("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+
+button10.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.add("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.remove("fade-in");
+});
+
+button11.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.add("fade-in");
+        image12.classList.remove("fade-in");
+});
+
+button12.addEventListener("click", function() 
+{
+        image1.classList.remove("fade-in");
+        image2.classList.remove("fade-in");
+        image3.classList.remove("fade-in");
+        image4.classList.remove("fade-in");
+        image5.classList.remove("fade-in");
+        image6.classList.remove("fade-in");
+        image7.classList.remove("fade-in");
+        image8.classList.remove("fade-in");
+        image9.classList.remove("fade-in");
+        image10.classList.remove("fade-in");
+        image11.classList.remove("fade-in");
+        image12.classList.add("fade-in");
+});
